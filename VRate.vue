308571513180df92fa0597acc0bb25ce7d15357c
anseoeun <53011370+anseoeun@users.noreply.github.com>
2021-03-04 00:12:50 +0900

    Add files via upload

---
+++ VRate.vue
@@ -0,0 +1,42 @@
+<template>
+    <el-rate
+     :value="value"
+      @input="onInput"
+      >
+
+    </el-rate>
+</template>
+
+<script>
+export default {
+  props: {
+    value: {
+      type: [String, Number],
+      default: ''
+    },
+
+  },
+  data() {
+    return {
+      val: this.value
+    }
+  },
+  watch: {
+    // value(newValue, oldValue) {
+    //   console.log(newValue, oldValue)
+    //   if (newValue !== oldValue) {
+    //     this.value = newValue
+    //     this.$emit('input', newValue)
+    //   }
+    // },
+  },
+  mounted(){
+    console.log(this.value)
+  },
+  methods: {
+    onInput(value) {
+      this.$emit('input', value)
+    },  
+  }
+}
+</script>
